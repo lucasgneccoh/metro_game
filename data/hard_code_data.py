7a6928d3b9982756f32002187264c5f9fa3c5c6e
lucasgneccoh <lucasgneccoh@gmail.com>
2023-11-15 15:18:58 +0100

    Deleted node modules, fixed file with line number

--- data/hard_code_data.py
+++ data/hard_code_data.py
@@ -1,3 +1,10 @@
 with open('cards_next_station_metro_only.txt', 'r') as f:
     for l in f:
         print(f'"{l.strip()}",')
+
+
+print('-'*30)
+
+with open('cards_line_number.txt', 'r') as f:
+    for l in f:
+        print(f'"{l.strip()}",')
